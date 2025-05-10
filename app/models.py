b8c7c3def4a365146491d2be82c04db43b4e0467
Mahesh Pidaparti <mrlsdevops>
2025-05-09 20:23:51 -0500

    Users

--- app/models.py
+++ app/models.py
@@ -12,3 +12,11 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+
+class User(Base):
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
